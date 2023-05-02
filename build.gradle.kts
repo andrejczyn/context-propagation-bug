89d13be40bac6b170bb8d60aa6cf1d437cf34a06
horhe <horhe@merxu.com>
2023-05-02 16:37:31 +0200

    bug example

---
+++ build.gradle.kts
@@ -0,0 +1,41 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.springframework.boot") version "3.0.5"
+    id("io.spring.dependency-management") version "1.1.0"
+    kotlin("jvm") version "1.7.22"
+    kotlin("plugin.spring") version "1.7.22"
+}
+
+group = "com.andrejczyn"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://repo.spring.io/snapshot") }
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("io.micrometer:micrometer-tracing-bridge-otel")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("ch.qos.logback.contrib:logback-json-classic:0.1.5")
+    implementation("io.github.microutils:kotlin-logging-jvm:2.1.23")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "17"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/main/kotlin/com/andrejczyn/DemoApplication.kt
@@ -0,0 +1,34 @@
+package com.andrejczyn
+
+import io.opentelemetry.api.trace.Span
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import mu.KotlinLogging
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.reactive.function.client.WebClient
+import reactor.core.publisher.Hooks
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+    Hooks.enableAutomaticContextPropagation()
+    runApplication<DemoApplication>(*args)
+}
+
+@RestController()
+class LogController {
+    val logger = KotlinLogging.logger {}
+
+    @GetMapping("/")
+    suspend fun log2() = coroutineScope {
+        val currentSpanBefore = Span.current()
+        logger.info { "Before ${Thread.currentThread().name} $currentSpanBefore" }
+        delay(10)
+        val currentSpanAfter = Span.current()
+        logger.info { "After ${Thread.currentThread().name} $currentSpanAfter" }
+    }
+}
