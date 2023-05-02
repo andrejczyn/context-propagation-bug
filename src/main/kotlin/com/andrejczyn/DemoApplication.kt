6c71800e311736f520f452d9e4d8d0010098e62f
horhe <horhe@merxu.com>
2023-05-02 16:37:44 +0200

    bug example

--- src/main/kotlin/com/andrejczyn/DemoApplication.kt
+++ src/main/kotlin/com/andrejczyn/DemoApplication.kt
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.reactive.function.client.WebClient
 import reactor.core.publisher.Hooks
 
 @SpringBootApplication
